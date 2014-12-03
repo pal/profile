8265818a93149c964fcbe0fa81327ed794eeb6fc
Peter Liljenberg <peter.liljenberg@pagero.com>
2014-12-03 14:49:59 +0100

    Added oh-my-git and init

--- home/.profile.d/zz_source_others.sh
+++ home/.profile.d/zz_source_others.sh
@@ -1,3 +1,3 @@
 source ~/tools/environment/environment.sh
-source ~/tools/oh-my-git/prompt.sh
+source "$HOME/.homesick/repos/profile/.oh-my-git/prompt.sh"
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
