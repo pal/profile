91bdc4f93942481d4a711e95124654f676a3db21
Peter Liljenberg <peter.liljenberg@pagero.com>
2014-12-03 14:52:26 +0100

    Updated link to oh-my-git

--- home/.profile.d/zz_source_others.sh
+++ home/.profile.d/zz_source_others.sh
@@ -1,3 +1,3 @@
 source ~/tools/environment/environment.sh
-source "$HOME/.homesick/repos/profile/.oh-my-git/prompt.sh"
+source "$HOME/.homesick/repos/profile/home/.oh-my-git/prompt.sh"
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
