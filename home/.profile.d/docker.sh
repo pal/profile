9c3240afc09f44e0dfe2f96743c8724db715a7eb
Peter Liljenberg <pl@speedledger.com>
2014-12-22 22:35:59 +0100

    Docker updates

--- home/.profile.d/docker.sh
+++ home/.profile.d/docker.sh
@@ -1,4 +1,6 @@
-export DOCKER_HOST=tcp://192.168.59.103:2375
+export DOCKER_CERT_PATH=/Users/pl/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
+export DOCKER_HOST=tcp://192.168.59.103:2376
 
 # Docker exec bash
 db() {
