acc8068d6d2d9629f6dc189f55d5f06411d9639e
Peter Liljenberg <pl@speedledger.com>
2015-01-08 23:42:10 +0100

    Update java

--- home/.profile.d/java.sh
+++ home/.profile.d/java.sh
@@ -1,14 +1,31 @@
-#JENV
-export JENV_ROOT=/usr/local/opt/jenv
-if which jenv > /dev/null; then
-    eval "$(jenv init -)"
-    jenv global 1.8
 
-    export JAVA_HOME="/usr/local/opt/jenv/versions/$(jenv global)"
-fi
-
-showjarversion() {
-  unzip -q -c $1 META-INF/MANIFEST.MF
+setjava() {
+    if [ -z "$1" ]; then
+        echo "Current JAVA_HOME: $JAVA_HOME"
+        echo "Current java: $(jenv version)"
+    else
+        jenv global $1
+        export JAVA_HOME=$(jenv javahome)
+    fi
 }
 
+showjarversion() {
+    unzip -q -c $1 META-INF/MANIFEST.MF
+}
 
+_setjava() {
+           local cur prev opts
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        prev="${COMP_WORDS[COMP_CWORD-1]}"
+        opts="$(jenv whence java)"
+
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+}
+
+complete -F _setjava setjava
+
+#JENV
+export JENV_ROOT=/usr/local/opt/jenv
+eval "$(jenv init -)"
+setjava 1.8
