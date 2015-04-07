3176d0fea8d9f017d04e0c9221c407d3bca265de
Pål Brattberg <brattberg@gmail.com>
2015-04-07 22:43:56 +0200

    Not developing SL2, remove config for this

--- home/.profile.d/zz_sl.sh
+++ home/.profile.d/zz_sl.sh
@@ -2,12 +2,4 @@
 #fix for locale issues when connecting to ubuntu servers
 export LANG="en_US.utf8"
 export LANGUAGE="en_US.utf8"
-#export LC_ALL="en_US.utf8"
 export LC_ALL=en_US.UTF-8
-
-CD=$(pwd)
-base sl
-cd $CD
-
-# SL2 config
-export slconfig=pal
